e108b15a57c46286a19e2e44de4a46377824f3e2
Molly Draven <drazi@duck.com>
2024-03-17 10:00:40 -0400

    Fix failing getTable test Fixes #6

--- test/getTable.spec.js
+++ test/getTable.spec.js
@@ -2,6 +2,7 @@ import assert from "node:assert";
 import { describe, it } from "node:test";
 // eslint-disable-next-line no-unused-vars
 import { getTable, Types } from "../index.js";
+import VerbDB from "../lib/database.js";
 
 describe("getTable()", () => {
   it("when called with a table that does not exist, should return a DBResult object, with an status set to 404, and an empty result key", () => {
@@ -11,7 +12,7 @@ describe("getTable()", () => {
     const expected = {
       status: 404,
       statusText: "Table Not Found",
-        resultCount: 0,
+      resultCount: 0,
       result: [],
     };
 
@@ -22,28 +23,38 @@ describe("getTable()", () => {
     assert.deepStrictEqual(result, expected);
   });
 
-    it("when called with a table that exists, should return a DBResult object, with an status set to 200, and a result key with the table object", () => {
-        // Arrange
-        const table = "users";
-        /** @type {Types.DBResult} */
-        const expected = {
-        status: 200,
-        statusText: "OK",
-        result: {
-            name: "users",
-            columns: ["id", "name", "email"],
-            rows: [
+  it("when called with a table that exists, should return a DBResult object, with an status set to 200, and a result key with the table object", () => {
+    // Arrange
+    const table = "users";
+    /** @type {Types.DBResult} */
+    const expected = {
+      status: 200,
+      statusText: "OK",
+      resultCount: 2,
+      result: {
+        name: "users",
+        columns: ["id", "name", "email"],
+        rows: [
+          { id: 1, name: "Alice", email: "" },
+          { id: 2, name: "Bob", email: "" },
+        ],
+        primaryKey: "id",
+      },
+    };
+    VerbDB.tables.set("users", {
+        name: "users",
+        columns: ["id", "name", "email"],
+        rows: [
             { id: 1, name: "Alice", email: "" },
             { id: 2, name: "Bob", email: "" },
-            ],
-            primaryKey: "id",
-        },
-        };
+        ],
+        primaryKey: "id",
+        });
 
-        // Act
-        const result = getTable(table);
+    // Act
+    const result = getTable(table);
 
-        // Assert
-        assert.deepStrictEqual(result, expected);
-    });
+    // Assert
+    assert.deepStrictEqual(result, expected);
+  });
 });
