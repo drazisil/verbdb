e3f44c3ea48a610ab09cd72dd93f719acda9c601
Molly Draven <drazi@duck.com>
2024-03-17 11:01:15 -0400

    Fix eslint errors regarding types Fixes #13

--- index.js
+++ index.js
@@ -14,6 +14,5 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-export * as Types from "./lib/types.js";
 export { getTable } from "./lib/getTable.js";
 export { headTable } from "./lib/headTable.js";
